9e9c0763229abadee5acddbcdbea57418580a483
Ievgen <alarr89@gmail.com>
2022-06-08 20:31:12 +0300

    Exersise 23

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,66 @@
-"use strict"
+// Место для первой задачи
+function firstTask() {
+    // Пишем решение вот тут
+    for(let i = 5; i < 11; i++) {
+        console.log(i);
+    }
+}
 
-let numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?','');
+// Место для второй задачи
+function secondTask() {
+    // Пишем решение вот тут
+    for(let i = 20; i > 9; i--) {
+        if(i === 13) {
+            break;
+        }
 
-let personalMowieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
-};
+        console.log(i);
+    }
+}
 
-let a = prompt('Один из последних просмотренных фильмов? 1',''),
-    b = prompt('На сколько оцените его?',''),
-    c = prompt('Один из последних просмотренных фильмов? 2',''),
-    d = prompt('На сколько оцените его?','');
+// Место для третьей задачи
+function thirdTask() {
+    // Пишем решение вот тут
+    for(let i = 2; i < 11; i+=2) {
+        console.log(i);
+    }
+}
 
-personalMowieDB.movies[a] = b;
-personalMowieDB.movies[c] = d;
+// Место для четвертой задачи
 
-console.log(personalMowieDB);
+// Цикл, который нужно переписать:
+
+// for (let i = 2; i <= 16; i++) {
+//     if (i % 2 === 0) {
+//         continue;
+//     } else {
+//         console.log(i);
+//     }
+// }
+
+function fourthTask() {
+    // Пишем решение вот тут
+    let i = 2;
+    while(i <= 16) {
+        if (i % 2 === 0) {
+            i++;
+            continue;
+        } 
+        console.log(i);
+        i++;
+    }
+}
+
+// Место для пятой задачи
+
+function fifthTask() {
+    const arrayOfNumbers = [];
+
+    // Пишем решение вот тут
+    for(let i = 5; i < 11; i++) {
+        arrayOfNumbers[i - 5] = i;
+    }
+        
+    // Не трогаем
+    return arrayOfNumbers;
+}
