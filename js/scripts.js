d5700079350bd8433b1064d76ac0050d50e37bb8
Ievgen <alarr89@gmail.com>
2022-06-11 18:28:43 +0300

    Exersise 23-3

--- js/scripts.js
+++ js/scripts.js
@@ -1,54 +1,32 @@
-function firstTask() {
-    // Значения массива менять нельзя, тут он проверяется автоматически именно на эти значения
-    const arr = [3, 5, 8, 16, 20, 23, 50];
-    const result = [];
-
-    // Пишем решение вот тут
-    for(let i = 0; i < arr.length; i++) {
-        result[i] = arr[i];
+const lines = 5;
+let result = '';
+// Проверяется именно переменная result, формируйте строку в ней
+for(let i = 0; i <= lines; i++) {
+    for(let j = 0; j < lines - i; j++) {
+        result += ' ';
     }
-    
-    // Не трогаем
-    return result;
+
+    for(let k = 0; k <= i; k++) {
+        result += '*';
+    }
+
+    for(let l = 0; l < i; l++) {
+        result += '*';
+    }
+
+    result += '\n';
 }
 
 
-// Место для второй задачи
-function secondTask() {
-    // Значения массива менять нельзя, тут он проверяется автоматически именно на эти значения
-    const data = [5, 10, 'Shopping', 20, 'Homework'];
+///// Или так /////
+// for (let i = 0; i <= lines; i++) {
+//     for (let j = 0; j < lines - i; j++) {
+//         result += " ";
+//     }
+//     for (let j = 0; j < 2 * i + 1; j++) {
+//         result += "*";
+//     }
+//     result += "\n";
+// }
 
-    // Пишем решение вот тут
-
-    for(let i = 0; i < data.length; i++) {
-        let itemType = typeof(data[i]);
-        if(itemType == 'number') {
-            data[i] *= 2;
-        } else {
-            data[i] = data[i] + ' - done';
-        }
-    }
-
-    // Не трогаем
-    return data;
-}
-
-
-// Место для третьей задачи
-function thirdTask() {
-    // Значения массива менять нельзя, тут он проверяется автоматически именно на эти значения
-    const data = [5, 10, 'Shopping', 20, 'Homework'];
-    const result = [];
-
-    // Пишем решение вот тут
-    let num = 0;
-    for(let i = data.length; i > 1; i--) {
-        result[num] = data[i];
-        num++;
-    }
-
-    console.log(result);
-    
-    // Не трогаем
-    return result;
-}
+console.log(result);
