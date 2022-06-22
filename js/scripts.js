28fa90c24e122293ae45cc3fe16c68b305f89ed4
Ievgen <alarr89@gmail.com>
2022-06-22 15:43:17 +0300

    29.3

--- js/scripts.js
+++ js/scripts.js
@@ -1,46 +1,26 @@
-function getTimeFromMinutes(minutesTotal) {
-    if (typeof(minutesTotal) !== 'number' || minutesTotal < 0 || !Number.isInteger(minutesTotal)) {
-        return "Ошибка, проверьте данные";
-    }
+function fib(number) {
+    
+    let result = [0, 1];
 
-    const hours = Math.floor(minutesTotal / 60);
-    const minutes = minutesTotal % 60;
+    if (typeof(number) !== 'number' || number <= 0 || !Number.isInteger(number)) {
 
-    let hoursStr = '';
-
-    switch (hours) {
-        case 0: 
-            hoursStr = 'часов';
-            break;
-        case 1:
-            hoursStr = 'час';
-            break;
-        case 2:
-        case 3:
-        case 4:
-            hoursStr = 'часа';
-            break;
-        default:
-            hoursStr = 'часов';
-    }
-
-    return `Это ${hours} ${hoursStr} и ${minutes} минут`;
-}
-
-getTimeFromMinutes(180)
-
-function findMaxNumber(a, b ,c, d) {
-    // Самое простое - это использовать Math.max :)
-    // А оптимизировать такую проверку мы научимся совсем скоро
-    if (typeof(a) !== 'number' ||
-        typeof(b) !== 'number' ||
-        typeof(c) !== 'number' ||
-        typeof(d) !== 'number') {
-        return 0;
+        return "";
+        
     } else {
-        return Math.max(a, b ,c, d);
-    }
-}
 
-findMaxNumber(1, 5, 6.6, 10.5);
-findMaxNumber(1, 5, '6', '10');
+        for(let i = 2; i <= number; i++) {
+            
+            result[i] = result[i - 1] + result[i - 2]; 
+        }
+
+        result = result.slice(0, number);
+
+        result = result.join(' ');
+    
+        return result;
+    }
+
+    
+
+
+}
