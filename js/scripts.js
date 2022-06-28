b82940b36afa72d12e4e43a6c3059e8b324917a1
Ievgen <alarr89@gmail.com>
2022-06-28 22:13:36 +0300

    Lesson 35

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,50 @@
 'use strict';
 
-const arr = [1, 2, 3, 6, 8];
+const objMain = {
+    a: 5,
+    b: 6,
+    c: 7
+};
 
-arr.forEach(function(item, i, arr) {
-    console.log(`${item} под номером ${i} из массива ${arr}`);
-});
+const objCopied = Object.assign({}, objMain);
 
-//arr.pop();
-//arr.push(10);
+objCopied.a = 1;
+objCopied.d = 8;
 
-//console.log(arr);
+console.log(objMain);
+console.log(objCopied);
 
-// for (let i = 0; i < arr.length; i++) {
-//     console.log(arr[i]);
-// }
+/* /////////////////////////////////// */
 
-// for (let value of arr) {
-//     console.log(value);
-// }
+let arrMain = ['a', 'b', 'c'];
+
+let arrCopied = arrMain.slice();
+
+arrCopied[0] = 'opa opa';
+
+console.log(arrMain);
+console.log(arrCopied);
+
+/* /////////////////////////////////// */
+
+const arrFirst = ['opa', 'popa', 'knopa'],
+      arrSecond = ['ipa', 'pipa', 'bipa'],
+      arrThird = [...arrFirst, ...arrSecond, 'kepa', 'pepa'];
+
+      console.log(arrThird);
+
+function threeElems(a, b, c) {
+    console.log(a);
+    console.log(b);
+    console.log(c);
+}
+
+threeElems(...arrFirst);
+
+/* /////////////////////////////////// */
+/* Получается, что копировать массивы и объекты можно просто с помощью фишки экмаскрипт 6 и 9, 
+spread Это название массива с тремя точечками */
+
+let arr1 = [1, 2, 3];
+let arr2 = [...arr1];
+console.log(arr2);
