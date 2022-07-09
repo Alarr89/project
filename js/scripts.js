701565b24e1b5c6f1b357f25b0dedbde4dc2cbee
Ievgen <alarr89@gmail.com>
2022-07-09 16:41:13 +0300

    Ex.41

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,24 @@
-const students = ['Peter', 'Andrew', 'Ann', 'Mark', 'Josh', 'Sandra', 'Cris', 'Bernard', 'Takesi', 'Sam'];
+•	Какое будет выведено значение: let x = 5; alert( x++ ); ? //5
 
-function sortStudentsByGroups(arr) {
+•	Чему равно такое выражение: [ ] + false - null + true ? //NaN
 
-    arr.sort();
+•	Что выведет этот код: let y = 1; let x = y = 2; alert(x); ? //2
 
-    let list = [],
-        a = [],
-        b = [],
-        c = [],
-        rest = [];
+•	Чему равна сумма [ ] + 1 + 2? //12
 
-    for(let i = 0; i < arr.length; i++) {
-        if(i < 3) {
-            a.push(arr[i]);
-        } else if(i > 2 && i < 6) {
-            b.push(arr[i]);
-        } else if(i > 5 && i < 9) {
-            c.push(arr[i]);
-        } else {
-            rest.push(arr[i]);
-        } 
-    }
+•	Что выведет этот код: alert( "1"[0] )? //1
 
-    list = [a, b, c, `Оставшиеся студенты: ${rest.length === 0 ? '-' : rest.join(', ')}`];
+•	Чему равно 2 && 1 && null && 0 && undefined ? //null
 
-    return list;
-}
+•	Есть ли разница между выражениями? // !!( a && b ) и (a && b)? //Да
+
+•	Что выведет этот код: alert( null || 2 && 3 || 4 ); ? //3
+
+•	a = [1, 2, 3]; b = [1, 2, 3]; Правда ли что a == b ? //нет
+
+•	Что выведет этот код: alert( +"Infinity" ); ? //Infinity
+
+•	Верно ли сравнение: "Ёжик" > "яблоко"? //нет
+
+•	Чему равно 0 || "" || 2 || undefined || true || falsе ? //2
+
