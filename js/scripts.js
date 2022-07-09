591a3b882b6093c97e06100b20db15f3478815c3
Ievgen <alarr89@gmail.com>
2022-07-09 17:20:50 +0300

    Ex.41 find errors

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,52 @@
-•	Какое будет выведено значение: let x = 5; alert( x++ ); ? //5
+const restorantData = {
+    menu: [
+        {
+            name: 'Salad Caesar',
+            price: '14$'
+        },
+        {
+            name: 'Pizza Diavola',
+            price: '9$'
+        },
+        {
+            name: 'Beefsteak',
+            price: '17$'
+        },
+        {
+            name: 'Napoleon',
+            price: '7$'
+        }
+    ],
+    waitors: [
+        {name: 'Alice', age: 22}, {name: 'John', age: 24}
+    ],
+    averageLunchPrice: '20$',
+    openNow: true
+};
 
-•	Чему равно такое выражение: [ ] + false - null + true ? //NaN
+function isOpen(prop) {
+    let answer = '';
+    prop ? answer = 'Открыто' : answer = 'Закрыто';
 
-•	Что выведет этот код: let y = 1; let x = y = 2; alert(x); ? //2
+    return answer;
+}
+console.log(isOpen(restorantData.openNow));
 
-•	Чему равна сумма [ ] + 1 + 2? //12
+function isAverageLunchPriceTrue(fDish, sDish, average) {
+    
+    if ((+fDish.price.slice(0,-1)) + (+sDish.price.slice(0,-1)) < +average.slice(0,-1)) {
+        return 'Цена ниже средней';
+    } else {
+        return 'Цена выше средней';
+    }
+}
+console.log(isAverageLunchPriceTrue(restorantData.menu[0], restorantData.menu[1], restorantData.averageLunchPrice));
 
-•	Что выведет этот код: alert( "1"[0] )? //1
+function transferWaitors(data) {
+    const copy = Object.assign({}, data);
 
-•	Чему равно 2 && 1 && null && 0 && undefined ? //null
+    copy.waitors = [{name: 'Mike', age: 32}];
+    return copy;
+}
 
-•	Есть ли разница между выражениями? // !!( a && b ) и (a && b)? //Да
-
-•	Что выведет этот код: alert( null || 2 && 3 || 4 ); ? //3
-
-•	a = [1, 2, 3]; b = [1, 2, 3]; Правда ли что a == b ? //нет
-
-•	Что выведет этот код: alert( +"Infinity" ); ? //Infinity
-
-•	Верно ли сравнение: "Ёжик" > "яблоко"? //нет
-
-•	Чему равно 0 || "" || 2 || undefined || true || falsе ? //2
-
+transferWaitors(restorantData);
