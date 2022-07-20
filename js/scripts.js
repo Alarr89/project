dda28b408b71567ff6559f3a38af5a798394d2b1
Ievgen <alarr89@gmail.com>
2022-07-20 23:17:54 +0300

    Ex.63

--- js/scripts.js
+++ js/scripts.js
@@ -1,106 +1,52 @@
-// function pow(x, n) {
-//     let result = 1;
+const btns = document.querySelectorAll('button'),
+      wrapper = document.querySelector('.wrapper');
 
-//     for (let i = 0; i < n; i++) {
-//         result *= x;
-//     }
+// console.log(btns[0].classList.length); // показывает количество классов
+// console.log(btns[0].classList.item[0]); // показывает определённый по порядку класс
+// console.log(btns[0].classList.add('red', 'one-more-class')); // добавляет класс
+// console.log(btns[0].classList.remove('red')); // удаляет класс
+// console.log(btns[0].classList.toggle('red')); // если класс есть, то удаляет. Если нет, то добавляет
 
-//     return result;
-// }
+// if (btns[1].classList.contains('red')) {
+//     console.log('red');
+// };
 
-// function pow(x, n) {
-//     if (n === 1) {
-//         return x;
-//     } else {
-//         return x * pow(x, n - 1);
-//     }
-// }
+// btns[0].addEventListener('click', () => {
+//     // if (!btns[1].classList.contains('red')) {
+//     //     btns[1].classList.add('red');
+//     // } else {
+//     //     btns[1].classList.remove('red');
+//     // }
 
-// console.log(pow(2,1));
-// console.log(pow(2,2));
-// console.log(pow(2,3));
-// console.log(pow(2,4));
-// console.log(pow(2,5));
+//     btns[1].classList.toggle('red');
+// });
 
-const students = {
-    js: [{
-       name: 'John',
-       progress: 100 
-    }, {
-        name: 'Ivy',
-        progress: 60
-    }],
+// ДЕЛЕГИРОВАНИЕ
 
-    html: {
-        basic: [{
-            name: 'Peter',
-            progress: 20 
-         }, {
-             name: 'Ann',
-             progress: 18
-         }],
-
-        pro: [{
-            name: 'Sam',
-            progress: 10
-        }]
-    }
-};
-
-function getProgressByIteration(data) {
-    let total = 0;
-    let students = 0;
-
-    for (let course of Object.values(data)) {
-        if(Array.isArray(course)) {
-            students += course.length;
-
-            for (let i = 0; i < course.length; i++) {
-                total += course[i].progress;
-            }
-        } else {
-            for (let subCourse of Object.values(data)) {
-                students += subCourse.length;
-
-                for (let i = 0; i < subCourse.length; i++) {
-                    total += subCourse[i].progress;
-                } 
-            }
-        }
-    }
-
-    return total / students;
-}
-
-// Задача сделать функцию которая рекурсией находит факториал
-
-function factorial(n) {
-    if (typeof(n) !== 'number' || !Number.isInteger(n)) {
-        return "Ошибка, проверьте данные";
-    }
-
-    if (n >= 1) {
-        return n * factorial(n - 1);
+wrapper.addEventListener('click', (event) => {
+    if(event.target && event.target.classlist.contains('red')) {
+        console.log('hello');
     } else {
-        return 1;
+        console.log('opa opa');
     }
-}
+});
 
-console.log(factorial(1.5));
+wrapper.addEventListener('click', (event) => {
+    if(event.target && event.target.classlist.matches('.red')) {
+        console.log('hello');
+    } else {
+        console.log('opa opa');
+    }
+});
 
+wrapper.addEventListener('click', (event) => {
+    if(event.target && event.target.classlist.tagname('BUTTON')) {
+        console.log('hello');
+    } else {
+        console.log('opa opa');
+    }
+});
 
-let arr = [1,2,3,4,5],
-    result = '';
-
-// for(let item in arr) {
-//     result += item + '\n';
-// }
-
-for(let i = 0; i < arr.length; i++) {
-    result = arr[i];
-}
-
-console.log(result);
-
-console.log(4 % 3);
-console.log(Math.sqrt(4));
+let newButton = document.createNewElement('button');
+newButton.classlist.add('red');
+wrapper.append(newButton);
